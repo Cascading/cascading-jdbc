fa4329f54b2ae262dbe3f632b741c5a54b602dd3
Andre Kelpe <efeshundertelf@googlemail.com>
2013-08-20 18:13:29 +0200

    added possibility to derive required TableDesc information from a Fields instance, which is useful, when we operate in the lingual/provider context.

--- cascading-jdbc-core/src/main/java/cascading/provider/jdbc/TableDesc.java
+++ cascading-jdbc-core/src/main/java/cascading/provider/jdbc/TableDesc.java
@@ -12,170 +12,249 @@
 
 package cascading.provider.jdbc;
 
-import cascading.util.Util;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import cascading.tuple.Fields;
+import cascading.util.Util;
+
+import com.google.common.collect.Lists;
+
 /**
- * Class TableDesc describes a SQL based table, this description is used by the {@link JDBCTap} when
- * creating a missing table.
- *
+ * Class TableDesc describes a SQL based table, this description is used by the
+ * {@link JDBCTap} when creating a missing table.
+ * 
  * @see JDBCTap
  * @see JDBCScheme
  */
-public class TableDesc implements Serializable {
-    /** Field tableName */
-    String tableName;
-    /** Field columnNames */
-    String[] columnNames;
-    /** Field columnDefs */
-    String[] columnDefs;
-    /** Field primaryKeys */
-    String[] primaryKeys;
+public class TableDesc implements Serializable
+  {
+  /** Field tableName */
+  String tableName;
+  /** Field columnNames */
+  String[] columnNames;
+  /** Field columnDefs */
+  String[] columnDefs;
+  /** Field primaryKeys */
+  String[] primaryKeys;
 
-    /**
-     * Constructor TableDesc creates a new TableDesc instance.
-     *
-     * @param tableName of type String
-     */
-    public TableDesc( String tableName ) {
-        this.tableName = tableName;
+  /**
+   * Constructor TableDesc creates a new TableDesc instance.
+   * 
+   * @param tableName of type String
+   */
+  public TableDesc(String tableName)
+    {
+    this.tableName = tableName;
     }
 
-    /**
-     * Constructor TableDesc creates a new TableDesc instance.
-     *
-     * @param tableName   of type String
-     * @param columnNames of type String[]
-     * @param columnDefs  of type String[]
-     * @param primaryKeys of type String
-     */
-    public TableDesc( String tableName, String[] columnNames, String[] columnDefs, String[] primaryKeys ) {
-        this.tableName = tableName;
-        this.columnNames = columnNames;
-        this.columnDefs = columnDefs;
-        this.primaryKeys = primaryKeys;
+  /**
+   * Constructor TableDesc creates a new TableDesc instance.
+   * 
+   * @param tableName of type String
+   * @param columnNames of type String[]
+   * @param columnDefs of type String[]
+   * @param primaryKeys of type String
+   */
+  public TableDesc(String tableName, String[] columnNames, String[] columnDefs, String[] primaryKeys)
+    {
+    this.tableName = tableName;
+    this.columnNames = columnNames;
+    this.columnDefs = columnDefs;
+    this.primaryKeys = primaryKeys;
     }
 
-    public String getTableName() {
-        return tableName;
+  public String getTableName()
+    {
+    return tableName;
     }
 
-    public String[] getColumnNames() {
-        return columnNames;
+  public String[] getColumnNames()
+    {
+    return columnNames;
     }
 
-    public String[] getColumnDefs() {
-        return columnDefs;
+  public String[] getColumnDefs()
+    {
+    return columnDefs;
     }
 
-    public String[] getPrimaryKeys() {
-        return primaryKeys;
+  public String[] getPrimaryKeys()
+    {
+    return primaryKeys;
     }
 
-    /**
-     * Method getTableCreateStatement returns the tableCreateStatement of this TableDesc object.
-     *
-     * @return the tableCreateStatement (type String) of this TableDesc object.
-     */
-    public String getCreateTableStatement() {
-        List<String> createTableStatement = new ArrayList<String>();
+  /**
+   * Method getTableCreateStatement returns the tableCreateStatement of this
+   * TableDesc object.
+   * 
+   * @return the tableCreateStatement (type String) of this TableDesc object.
+   */
+  public String getCreateTableStatement()
+    {
+    List<String> createTableStatement = new ArrayList<String>();
 
-        createTableStatement = addCreateTableBodyTo( createTableStatement );
+    createTableStatement = addCreateTableBodyTo( createTableStatement );
 
-        return String.format( getCreateTableFormat(), tableName, Util.join( createTableStatement, ", " ) );
+    return String.format( getCreateTableFormat(), tableName, Util.join( createTableStatement, ", " ) );
     }
 
-    protected List<String> addCreateTableBodyTo( List<String> createTableStatement ) {
-        createTableStatement = addDefinitionsTo( createTableStatement );
-        createTableStatement = addPrimaryKeyTo( createTableStatement );
+  protected List<String> addCreateTableBodyTo(List<String> createTableStatement)
+    {
+    createTableStatement = addDefinitionsTo( createTableStatement );
+    createTableStatement = addPrimaryKeyTo( createTableStatement );
 
-        return createTableStatement;
+    return createTableStatement;
     }
 
-    protected String getCreateTableFormat() {
-        return "CREATE TABLE %s ( %s )";
+  protected String getCreateTableFormat()
+    {
+    return "CREATE TABLE %s ( %s )";
     }
 
-    protected List<String> addDefinitionsTo( List<String> createTableStatement ) {
-        for( int i = 0; i < columnNames.length; i++ ) {
-            String columnName = columnNames[ i ];
-            String columnDef = columnDefs[ i ];
+  protected List<String> addDefinitionsTo(List<String> createTableStatement)
+    {
+    for ( int i = 0; i < columnNames.length; i++ )
+      {
+      String columnName = columnNames[i];
+      String columnDef = columnDefs[i];
 
-            createTableStatement.add( columnName + " " + columnDef );
+      createTableStatement.add( columnName + " " + columnDef );
+      }
+
+    return createTableStatement;
+    }
+
+  protected List<String> addPrimaryKeyTo(List<String> createTableStatement)
+    {
+    if ( hasPrimaryKey() )
+      createTableStatement.add( String.format( "PRIMARY KEY( %s )", Util.join( primaryKeys, ", " ) ) );
+
+    return createTableStatement;
+    }
+
+  /**
+   * Method getTableDropStatement returns the tableDropStatement of this
+   * TableDesc object.
+   * 
+   * @return the tableDropStatement (type String) of this TableDesc object.
+   */
+  public String getTableDropStatement()
+    {
+    return String.format( getDropTableFormat(), tableName );
+    }
+
+  protected String getDropTableFormat()
+    {
+    return "DROP TABLE %s";
+    }
+
+  /**
+   * Method getTableExistsQuery returns the tableExistsQuery of this TableDesc
+   * object.
+   * 
+   * @return the tableExistsQuery (type String) of this TableDesc object.
+   */
+  public String getTableExistsQuery()
+    {
+    return String.format( "select 1 from %s where 1 = 0", tableName );
+    }
+
+  private boolean hasPrimaryKey()
+    {
+    return primaryKeys != null && primaryKeys.length != 0;
+    }
+
+  /**
+   * Determines if the instance has a useful tablename, columns and column
+   * descriptions set. Useful means, that they are non-null and not empty and
+   * for each column, there is a type definition.
+   * 
+   * @return Returns <code>true</code> if all requirements are met, otherwise
+   *         <code>false</code>.
+   * */
+  public boolean hasRequiredTableInformation()
+    {
+    return tableName != null && !tableName.isEmpty() && columnNames != null && columnNames.length > 0 && columnDefs != null
+        && columnNames.length > 0 && columnDefs.length == columnNames.length;
+    }
+
+  /**
+   * This method can be used to fill in the required column names and
+   * descriptions from a Fields instance. This can be useful, when the types can
+   * only be determined after a flow has been started. This mechanism can be
+   * used to simplify the usage of the {@link JDBCTap} and {@link JDBCScheme} as
+   * a provider. The method
+   * 
+   * @param fields The {@link Fields} instance to derive the table structure
+   *          from.
+   * 
+   * @throws IllegalArgumentException In case the instance is still incomplete,
+   *           after trying to determine the table structure fromt he given
+   *           fields.
+   */
+  public void completeFromFields(Fields fields)
+    {
+    if ( !hasRequiredTableInformation() )
+      {
+      List<String> names = Lists.newArrayList();
+      List<String> defs = Lists.newArrayList();
+
+      for ( int i = 0; i < fields.size(); i++ )
+        {
+        Comparable<?> cmp = fields.get( i );
+        names.add( cmp.toString() );
+        defs.add( FieldsTypeMapping.sqltypeForClass( fields.getTypeClass( cmp ) ) );
         }
-
-        return createTableStatement;
+      columnNames = names.toArray( new String[names.size()] );
+      columnDefs = defs.toArray( new String[defs.size()] );
+      
+      // now it has to be complete and usable, if not bail out.
+      if ( !hasRequiredTableInformation() )
+        throw new IllegalStateException( "could not derive TableDesc from given fields." );
+      }
     }
 
-    protected List<String> addPrimaryKeyTo( List<String> createTableStatement ) {
-        if( hasPrimaryKey() )
-            createTableStatement.add( String.format( "PRIMARY KEY( %s )", Util.join( primaryKeys, ", " ) ) );
-
-        return createTableStatement;
+  @Override
+  public String toString()
+    {
+    return "TableDesc{" + "tableName='" + tableName + '\'' + ", columnNames="
+        + ( columnNames == null ? null : Arrays.asList( columnNames ) ) + ", columnDefs="
+        + ( columnDefs == null ? null : Arrays.asList( columnDefs ) ) + ", primaryKeys="
+        + ( primaryKeys == null ? null : Arrays.asList( primaryKeys ) ) + '}';
     }
 
-    /**
-     * Method getTableDropStatement returns the tableDropStatement of this TableDesc object.
-     *
-     * @return the tableDropStatement (type String) of this TableDesc object.
-     */
-    public String getTableDropStatement() {
-        return String.format( getDropTableFormat(), tableName );
+  @Override
+  public boolean equals(Object object)
+    {
+    if ( this == object )
+      return true;
+    if ( ! ( object instanceof TableDesc ) )
+      return false;
+
+    TableDesc tableDesc = (TableDesc) object;
+
+    if ( !Arrays.equals( columnDefs, tableDesc.columnDefs ) )
+      return false;
+    if ( !Arrays.equals( columnNames, tableDesc.columnNames ) )
+      return false;
+    if ( !Arrays.equals( primaryKeys, tableDesc.primaryKeys ) )
+      return false;
+    if ( tableName != null ? !tableName.equals( tableDesc.tableName ) : tableDesc.tableName != null )
+      return false;
+
+    return true;
     }
 
-    protected String getDropTableFormat() {
-        return "DROP TABLE %s";
+  @Override
+  public int hashCode()
+    {
+    int result = tableName != null ? tableName.hashCode() : 0;
+    result = 31 * result + ( columnNames != null ? Arrays.hashCode( columnNames ) : 0 );
+    result = 31 * result + ( columnDefs != null ? Arrays.hashCode( columnDefs ) : 0 );
+    result = 31 * result + ( primaryKeys != null ? Arrays.hashCode( primaryKeys ) : 0 );
+    return result;
     }
-
-    /**
-     * Method getTableExistsQuery returns the tableExistsQuery of this TableDesc object.
-     *
-     * @return the tableExistsQuery (type String) of this TableDesc object.
-     */
-    public String getTableExistsQuery() {
-        return String.format( "select 1 from %s where 1 = 0", tableName );
-    }
-
-    private boolean hasPrimaryKey() {
-        return primaryKeys != null && primaryKeys.length != 0;
-    }
-
-    @Override
-    public String toString() {
-        return "TableDesc{" + "tableName='" + tableName + '\'' + ", columnNames=" + ( columnNames == null ? null : Arrays.asList( columnNames ) ) + ", columnDefs=" + ( columnDefs == null ? null : Arrays.asList( columnDefs ) ) + ", primaryKeys=" + ( primaryKeys == null ? null : Arrays.asList( primaryKeys ) ) + '}';
-    }
-
-    @Override
-    public boolean equals( Object object ) {
-        if( this == object )
-            return true;
-        if( !( object instanceof TableDesc ) )
-            return false;
-
-        TableDesc tableDesc = (TableDesc) object;
-
-        if( !Arrays.equals( columnDefs, tableDesc.columnDefs ) )
-            return false;
-        if( !Arrays.equals( columnNames, tableDesc.columnNames ) )
-            return false;
-        if( !Arrays.equals( primaryKeys, tableDesc.primaryKeys ) )
-            return false;
-        if( tableName != null ? !tableName.equals( tableDesc.tableName ) : tableDesc.tableName != null )
-            return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = tableName != null ? tableName.hashCode() : 0;
-        result = 31 * result + ( columnNames != null ? Arrays.hashCode( columnNames ) : 0 );
-        result = 31 * result + ( columnDefs != null ? Arrays.hashCode( columnDefs ) : 0 );
-        result = 31 * result + ( primaryKeys != null ? Arrays.hashCode( primaryKeys ) : 0 );
-        return result;
-    }
-}
+  }
---
+++ cascading-jdbc-core/src/test/java/cascading/provider/jdbc/TableDescTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2007-2013 Concurrent, Inc. All Rights Reserved.
+ *
+ * Project and contact information: http://www.cascading.org/
+ *
+ * This file is part of the Cascading project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cascading.provider.jdbc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cascading.tuple.Fields;
+
+public class TableDescTest
+  {
+
+  @Test
+  public void testHasRequiredTableInformation()
+    {
+    TableDesc desc = new TableDesc( "name" );
+    assertFalse( desc.hasRequiredTableInformation() );
+
+    desc = new TableDesc( "name", null, null, null );
+    assertFalse( desc.hasRequiredTableInformation() );
+
+    desc = new TableDesc( "name", new String[] { "id" }, null, null );
+    assertFalse( desc.hasRequiredTableInformation() );
+
+    desc = new TableDesc( "name", new String[] { "id" }, new String[] { "int" }, null );
+    assertTrue( desc.hasRequiredTableInformation() );
+
+    }
+
+  @Test
+  public void testCompleteFromFields()
+    {
+    TableDesc desc = new TableDesc( "name" );
+    assertFalse( desc.hasRequiredTableInformation() );
+
+    Fields fields = new Fields( "id", int.class );
+    desc.completeFromFields( fields );
+
+    assertTrue( desc.hasRequiredTableInformation() );
+
+    assertArrayEquals( new String[] { "id" }, desc.getColumnNames() );
+
+    assertArrayEquals( new String[] { "int not null" }, desc.getColumnDefs() );
+
+    }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testCompleteFromFieldsMissingType()
+    {
+    TableDesc desc = new TableDesc( "name" );
+    assertFalse( desc.hasRequiredTableInformation() );
+
+    Fields fields = new Fields( "id" );
+    desc.completeFromFields( fields );
+    }
+
+  @Test(expected = IllegalStateException.class)
+  public void testCompleteFromFieldsWithUnknownFields()
+    {
+    TableDesc desc = new TableDesc( "name" );
+    assertFalse( desc.hasRequiredTableInformation() );
+
+    Fields fields = Fields.UNKNOWN;
+    desc.completeFromFields( fields );
+    }
+
+  }
