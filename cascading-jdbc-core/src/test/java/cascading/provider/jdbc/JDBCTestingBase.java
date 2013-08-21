99f0924d2e0e3adcef1ed832126ebdae67f5fdcb
Andre Kelpe <efeshundertelf@googlemail.com>
2013-08-21 14:04:19 +0200

    copyright header

--- cascading-jdbc-core/src/test/java/cascading/provider/jdbc/JDBCTestingBase.java
+++ cascading-jdbc-core/src/test/java/cascading/provider/jdbc/JDBCTestingBase.java
@@ -1,13 +1,21 @@
 /*
- * Copyright (c) 2013 Concurrent, Inc.
+ * Copyright (c) 2007-2013 Concurrent, Inc. All Rights Reserved.
  *
- * This work has been released into the public domain
- * by the copyright holder. This applies worldwide.
+ * Project and contact information: http://www.cascading.org/
  *
- * In case this is not legally possible:
- * The copyright holder grants any entity the right
- * to use this work for any purpose, without any
- * conditions, unless such conditions are required by law.
+ * This file is part of the Cascading project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package cascading.provider.jdbc;
